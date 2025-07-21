580b16fc76a78f84e3a341d9528ea69d28aa4e17
Eric Kohler <erickohlerdev@gmail.com>
2025-07-20 17:22:28 -0700

    handle hover over other list

--- app/index.tsx
+++ app/index.tsx
@@ -14,6 +14,7 @@ import Animated, {
 } from 'react-native-reanimated';
 
 const ITEM_HEIGHT = 70;
+const LIST_PADDING = 20;
 
 type Workout = { id: number; title: string };
 const initialWorkout1: Workout[] = [
@@ -30,7 +31,6 @@ const initialWorkout2: Workout[] = [
 type ListType = 'A' | 'B';
 
 export default function EditWorkouts() {
-    const listPadding = 20;
 
     const [listA, setListA] = useState<Workout[]>(initialWorkout1);
     const [listB, setListB] = useState<Workout[]>(initialWorkout2);
@@ -44,7 +44,7 @@ export default function EditWorkouts() {
 
     const activeId = useSharedValue<number | null>(null);
     const translateY = useSharedValue(0);
-    const activeList = useSharedValue<ListType>('A');    
+    const activeList = useSharedValue<ListType | null>(null);
 
     const reorder = (list: Workout[], order: SharedValue<number[]>) =>
         order.value
@@ -58,6 +58,58 @@ export default function EditWorkouts() {
         requestAnimationFrame(() => {
             activeId.value = null;
         });
+    };
+
+    const handleHover = (currentY: number, item: Workout) => {
+        const hoverA =
+            layoutA.value &&
+            currentY >= layoutA.value.y &&
+            currentY <= layoutA.value.y + layoutA.value.height;
+
+        const hoverB =
+            layoutB.value &&
+            currentY >= layoutB.value.y &&
+            currentY <= layoutB.value.y + layoutB.value.height;
+
+        const currentListID = activeList.value;
+
+        const destListID = hoverA ? 'A' : hoverB ? 'B' : currentListID;
+        const destOrder = currentListID === 'A' ? orderB : orderA;
+        const destLayout = currentListID === 'A' ? layoutB : layoutA;
+
+        console.log(destOrder.value);
+
+        // IF the destination layout does NOT exist, return
+        if (!destLayout.value) return;
+
+        const cloneID = 0;
+        const hasClone = destOrder.value.includes(cloneID);
+
+        // IF does NOT have clone and NOT hovering the list, no action needed, return
+        if (!hasClone && currentListID === destListID) return;
+
+        // IF does have clone, but NOT hovering the list, remove the clone id from the destination order, return
+        if (hasClone && currentListID === destListID) {
+            destOrder.value = destOrder.value.filter(id => id !== cloneID); // Remove zero          
+            return;
+        }
+
+        // IS hovering the desitination list, 
+        // Add the clone IF not already in the destination order
+        const newDestOrder = !hasClone ? [...destOrder.value, cloneID] : [...destOrder.value];
+
+        // Update the clone's order position in the destination list
+        const cloneIndex = newDestOrder.indexOf(cloneID); // Find the index of the clone
+        const newCloneIndex = Math.max(0, Math.min(
+            Math.floor((currentY - destLayout.value.y - LIST_PADDING) / ITEM_HEIGHT),
+            newDestOrder.length - 1
+        ));
+
+        if (newCloneIndex !== cloneIndex) {
+            newDestOrder.splice(cloneIndex, 1);
+            newDestOrder.splice(newCloneIndex, 0, cloneID);
+            destOrder.value = newDestOrder;
+        }
     };
 
     const handleDrop = (currentY: number, item: Workout) => {
@@ -80,21 +132,32 @@ export default function EditWorkouts() {
         const updatedSource = sourceList.filter(w => w.id !== item.id);
         const updatedDest = (destList === 'A' ? listA : listB).concat(item);
 
+        const cloneID = 0;
+
         if (currentList === 'A') {
             orderA.value = updatedSource.map(w => w.id);
-            orderB.value = updatedDest.map(w => w.id);
+
+            const cloneIndex = orderB.value.indexOf(cloneID);
+            const newOrder = [...orderB.value];
+            newOrder[cloneIndex] = item.id;
+            orderB.value = newOrder;
 
             setListA(updatedSource);
             setListB(updatedDest);
+
         } else {
-            orderA.value = updatedDest.map(w => w.id);
             orderB.value = updatedSource.map(w => w.id);
+
+            const cloneIndex = orderA.value.indexOf(cloneID);
+            const newOrder = [...orderA.value];
+            newOrder[cloneIndex] = item.id;
+            orderA.value = newOrder;
 
             setListA(updatedDest);
             setListB(updatedSource);
-        }
 
-        translateY.value = updatedDest.length * ITEM_HEIGHT - ITEM_HEIGHT;
+            // translateY.value = cloneIndex * ITEM_HEIGHT + LIST_PADDING;
+        }
 
         requestAnimationFrame(() => {
             activeId.value = null;
@@ -113,15 +176,15 @@ export default function EditWorkouts() {
         layout: SharedValue<LayoutRectangle | null>;
     }) => {
         const animatedStyle = useAnimatedStyle(() => {
-            const isActive = activeId.value === item.id;
+            const isActive = activeId.value === item.id && activeList.value === listType;
             const currentIndex = order.value.indexOf(item.id);
-            const targetY = currentIndex * ITEM_HEIGHT + listPadding;
+            const targetY = currentIndex * ITEM_HEIGHT + LIST_PADDING;
 
             return {
                 position: 'absolute',
                 top: 0,
-                left: listPadding,
-                right: listPadding,
+                left: LIST_PADDING,
+                right: LIST_PADDING,
                 height: ITEM_HEIGHT,
                 zIndex: isActive ? 100 : 0,
                 opacity: isActive ? 0.8 : 1,
@@ -136,7 +199,7 @@ export default function EditWorkouts() {
 
         const gesture = Gesture.Pan()
             .onStart(e => {
-                if(!layout || !layout.value) return;
+                if (!layout || !layout.value) return;
 
                 activeId.value = item.id;
                 activeList.value = listType;
@@ -146,17 +209,17 @@ export default function EditWorkouts() {
                 translateY.value = touchY - (ITEM_HEIGHT / 2) - layout.value.y;
             })
             .onUpdate(e => {
-                if(!layout || !layout.value) return;
+                if (!layout || !layout.value) return;
 
                 const currentY = e.absoluteY;
 
                 translateY.value = currentY - (ITEM_HEIGHT / 2) - layout.value.y;
 
-                console.log("Current Y: ", currentY, "Translate Y: ", translateY.value);
+                // console.log("Current Y: ", currentY, "Translate Y: ", translateY.value);
 
                 const currentIndex = order.value.indexOf(item.id);
                 const newIndex = Math.max(0, Math.min(
-                    Math.floor((currentY - layout.value.y - listPadding) / ITEM_HEIGHT),
+                    Math.floor((currentY - layout.value.y - LIST_PADDING) / ITEM_HEIGHT),
                     order.value.length - 1
                 ));
 
@@ -166,6 +229,8 @@ export default function EditWorkouts() {
                     newOrder.splice(newIndex, 0, item.id);
                     order.value = newOrder;
                 }
+
+                runOnJS(handleHover)(e.absoluteY, item);
             })
             .onEnd(e => {
                 runOnJS(handleDrop)(e.absoluteY, item);
@@ -188,8 +253,18 @@ export default function EditWorkouts() {
 
         const newOrder = reorder(list, order);
 
+        const animatedStyle = useAnimatedStyle(() => {
+            const height = order.value.length * ITEM_HEIGHT + (LIST_PADDING * 2);
+
+            return {
+                height: withTiming(height),
+                minHeight: ITEM_HEIGHT + (LIST_PADDING * 2),
+                borderColor: "#fff", borderRadius: 20, borderWidth: 2
+            };
+        });
+
         return (
-            <View
+            <Animated.View
                 onLayout={e => {
                     const layout = e.nativeEvent.layout;
                     if (listType === 'A') {
@@ -198,21 +273,17 @@ export default function EditWorkouts() {
                         layoutB.value = layout;
                     }
                 }}
-                style={{
-                    height: newOrder.length * ITEM_HEIGHT + (listPadding * 2), 
-                    minHeight: ITEM_HEIGHT + (listPadding * 2),
-                    borderColor: "#fff", borderRadius: 20, borderWidth: 2
-                }}>
-                {newOrder.map((item) => (
+                style={animatedStyle}>
+                {newOrder.map((item, index) => (
                     <WorkoutItem
-                        key={item.id}
+                        key={`${item.id}-${index}-${listType}`}
                         item={item}
                         listType={listType}
                         order={order}
                         layout={listType === 'A' ? layoutA : layoutB}
                     />
                 ))}
-            </View>
+            </Animated.View>
         );
     };
 
@@ -221,7 +292,7 @@ export default function EditWorkouts() {
             <Text style={styles.heading}>List A</Text>
             {renderList(listA, orderA, 'A')}
 
-            <Text style={[styles.heading, {marginTop: 40}]}>List B</Text>
+            <Text style={[styles.heading, { marginTop: 40 }]}>List B</Text>
             {renderList(listB, orderB, 'B')}
         </GestureHandlerRootView>
     );
